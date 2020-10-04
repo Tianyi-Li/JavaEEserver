4296570572fa4a11fe9ee9b054bee9e6aed9871a
Tianyi-Li <tianyityl@gmail.com>
2020-10-03 20:23:09 -0400

    week2 lab3

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Vendor (Address1,City,Province,PostalCode,Phone,Type,Name,Email)
+VALUES ('123 Maple St','London','On', 'N1N-1N1','(555)555-5555','Trusted','ABC Supply Co.','abc@supply.com');
+INSERT INTO Vendor (Address1,City,Province,PostalCode,Phone,Type,Name,Email)
+VALUES ('543 Sycamore Ave','Toronto','On', 'N1P-1N1','(999)555-5555','Trusted','Big Bills Depot','bb@depot.com');
+INSERT INTO Vendor (Address1,City,Province,PostalCode,Phone,Type,Name,Email)
+VALUES ('922 Oak St','London','On', 'N1N-1N1','(555)555-5599','Un Trusted','Shady Sams','ss@underthetable.com');
+INSERT INTO Vendor (Address1,City,Province,PostalCode,Phone,Type,Name,Email)
+VALUES ('1601 Richmond St','London','On', 'N7K-3W5','(519)666-6978','Un Trusted','Tianyi Li','tyl@gmail.com');
+
