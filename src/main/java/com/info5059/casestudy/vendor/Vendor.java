ad19e527e5e5b623637e331c4988cd626cfd5f3d
Tianyi-Li <tianyityl@gmail.com>
2020-10-17 16:20:23 -0400

    Week5 Lab8 Finished

--- src/main/java/com/info5059/casestudy/vendor/Vendor.java
+++ src/main/java/com/info5059/casestudy/vendor/Vendor.java
@@ -1,14 +1,18 @@
 package com.info5059.casestudy.vendor;
 
+import com.info5059.casestudy.product.Product;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
 @RequiredArgsConstructor
 public class Vendor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long Id;
@@ -20,6 +24,9 @@ public class Vendor {
     private String phone;
     private String type;
     private String email;
+
+    @OneToMany(mappedBy = "vendorid", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Product> products = new HashSet<>();
 }
 
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,4 +6,20 @@ INSERT INTO Vendor (Address1,City,Province,PostalCode,Phone,Type,Name,Email)
 VALUES ('922 Oak St','London','On', 'N1N-1N1','(555)555-5599','Untrusted','Shady Sams','ss@underthetable.com');
 INSERT INTO Vendor (Address1,City,Province,PostalCode,Phone,Type,Name,Email)
 VALUES ('1601 Richmond St','London','On', 'N7K-3W5','(519)666-6978','Untrusted','Tianyi Li','tyl@gmail.com');
-
+-- add Product
+INSERT INTO Product (ID, VendorId, Name, CostPrice, MSRP, ROP, EOQ, QOH, QOO, QRCode, QRCodeTxt)
+VALUES ('bdi001', 1, 'Bacardi', 19.99, 24.99, 5, 2, 20, 0, '', '');
+INSERT INTO Product (ID, VendorId, Name, CostPrice, MSRP, ROP, EOQ, QOH, QOO, QRCode, QRCodeTxt)
+VALUES ('jd001', 1, 'Jack Daniels', 29.99, 39.99, 10, 3, 15, 0, '', '');
+INSERT INTO Product (ID, VendorId, Name, CostPrice, MSRP, ROP, EOQ, QOH, QOO, QRCode, QRCodeTxt)
+VALUES ('hn001', 1, 'Hennessy', 69.99, 99.99, 5, 4, 15, 0, '', '');
+INSERT INTO Product (ID, VendorId, Name, CostPrice, MSRP, ROP, EOQ, QOH, QOO, QRCode, QRCodeTxt)
+VALUES ('rb001', 2, 'Root Beer', 2.99, 5.99, 15, 1, 20, 0, '', '');
+INSERT INTO Product (ID, VendorId, Name, CostPrice, MSRP, ROP, EOQ, QOH, QOO, QRCode, QRCodeTxt)
+VALUES ('up001', 2, '7UP', 2.99, 5.99, 20, 1, 20, 0, '', '');
+INSERT INTO Product (ID, VendorId, Name, CostPrice, MSRP, ROP, EOQ, QOH, QOO, QRCode, QRCodeTxt)
+VALUES ('pp001', 2, 'Pepsi', 1.99, 4.99, 25, 1, 30, 0, '', '');
+INSERT INTO Product (ID, VendorId, Name, CostPrice, MSRP, ROP, EOQ, QOH, QOO, QRCode, QRCodeTxt)
+VALUES ('hb001', 3, 'Coffee Maker', 119.99, 159.99, 5, 16, 3, 0, '', '');
+INSERT INTO Product (ID, VendorId, Name, CostPrice, MSRP, ROP, EOQ, QOH, QOO, QRCode, QRCodeTxt)
+VALUES ('bs001', 4, 'Vacuum', 139.99, 209.99, 5, 20, 5, 0, '', '');
